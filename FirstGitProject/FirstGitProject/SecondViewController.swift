4dca9b0cb661a6e3d2fc2b3994d73c8c6fa62312
Карим Хасбулатов <ismaisan@gmail.com>
2018-05-30 14:51:05 +0300

    add second view controller

---
+++ FirstGitProject/FirstGitProject/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  FirstGitProject
+//
+//  Created by Khasbulatov on 30.05.2018.
+//  Copyright © 2018 testOrganization. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
